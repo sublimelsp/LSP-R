7eb7ec414434a94823ee51d0c5845b7d04ac3607
Randy Lai <randy.cs.lai@gmail.com>
2021-06-08 12:23:24 -0700

    remove the custom default R binary command for now

--- plugin.py
+++ plugin.py
@@ -5,23 +5,6 @@ from LSP.plugin import unregister_plugin
 from LSP.plugin import AbstractPlugin
 
 
-def get_r_binary():
-    r_binary = None
-    window = sublime.active_window()
-    if window:
-        view = window.active_view()
-        if view:
-            r_ide_project_settings = view.settings().get("R-IDE", {})
-            if "r_binary" in r_ide_project_settings:
-                r_binary = r_ide_project_settings["r_binary"]
-    if not r_binary:
-        ride_settings = sublime.load_settings("R-IDE.sublime-settings")
-        r_binary = ride_settings.get("r_binary")
-    if not r_binary:
-        r_binary = "R"
-    return r_binary
-
-
 class LspRPlugin(AbstractPlugin):
     @classmethod
     def name(cls):
@@ -33,14 +16,8 @@ class LspRPlugin(AbstractPlugin):
         filepath = "Packages/LSP-R/{}".format(basename)
         return sublime.load_settings(basename), filepath
 
-    @classmethod
-    def additional_variables(cls):
-        return {
-            "r_binary": get_r_binary()
-        }
 
-
-class rlangPlugin(AbstractPlugin):
+class DeprecatedRlangPlugin(AbstractPlugin):
     @classmethod
     def name(cls):
         return "rlang"
@@ -55,9 +32,9 @@ class rlangPlugin(AbstractPlugin):
 
 def plugin_loaded():
     register_plugin(LspRPlugin)
-    register_plugin(rlangPlugin)
+    register_plugin(DeprecatedRlangPlugin)
 
 
 def plugin_unloaded():
     unregister_plugin(LspRPlugin)
-    unregister_plugin(rlangPlugin)
+    unregister_plugin(DeprecatedRlangPlugin)
