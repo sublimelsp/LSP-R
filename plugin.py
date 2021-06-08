c6ba41e1eb90ec1f894e443ae99ec95b9abb0ff0
Randy Lai <randy.cs.lai@gmail.com>
2021-06-07 23:41:12 -0700

    force rlang to disable

--- plugin.py
+++ plugin.py
@@ -22,9 +22,23 @@ class LspRPlugin(AbstractPlugin):
         return {"r_binary": "R"}
 
 
+class rlangPlugin(AbstractPlugin):
+    @classmethod
+    def name(cls):
+        return "rlang"
+
+    @classmethod
+    def configuration(cls):
+        basename = "rlang.hidden-sublime-settings"
+        filepath = "Packages/LSP-R/{}".format(basename)
+        return sublime.load_settings(basename), filepath
+
+
 def plugin_loaded():
     register_plugin(LspRPlugin)
+    register_plugin(rlangPlugin)
 
 
 def plugin_unloaded():
     unregister_plugin(LspRPlugin)
+    unregister_plugin(rlangPlugin)
